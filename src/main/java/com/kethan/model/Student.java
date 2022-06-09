9ec288bd642d5a33c70ee41e78bd1a4b5435c050
Jeevani <itsjeevani@gmail.com>
2022-06-09 12:28:14 -0400

    Added a query

--- src/main/java/com/kethan/model/Student.java
+++ src/main/java/com/kethan/model/Student.java
@@ -1,18 +1,28 @@
 package com.kethan.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
 
 @Entity
+@Table(name = "student", 
+       uniqueConstraints = @UniqueConstraint(
+    		               name = "emailid_unique", 
+    		               columnNames = "emailId"
+    		               )
+        )
 public class Student {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private long studentId;
 	private String firstName;
 	private String lastName;
+	@Column(nullable = false)
 	private String emailId;
 
 	public long getStudentId() {
